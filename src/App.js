e3f3c653f90204ddd5c234d81f39c55983fc8f6f
dazedcodes <techwithmaya@gmail.com>
2021-12-27 16:41:25 -0600

    Add: front-end functionality of stock CRUD based app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import './App.css';
 import React, { Component } from 'react'
 
 export class App extends Component {
-  // Javascript Code Here 
+  // App Constructor
   constructor(props) {
     super(props);
-    this.stocks = [];
-    this.state = {ssymbol: "", scategory: "", sshares: "", splatform: ""};
+    this.state = {stocks: [], ssymbol: "", scategory: "", sshares: "", splatform: "", currId: ""};
   }
+  // handleEdit: switch "Add" button to "Save" + track the ID of stock to edit 
+  handleEdit(event) {
+    document.getElementById("add-btn").innerHTML = "Save";
+    this.setState({currId: event.target.id});
+    event.preventDefault();
+  }
+  // handleDelete: removes stock object from array tracked in state
+  handleDelete(event){
+    event.preventDefault();
+    let filteredArray = this.state.stocks.filter(stock => stock.id != event.target.id);
+    this.setState({stocks: filteredArray});
+  }
+  // handleReset: resets the input fields to empty 
+  handleReset() {
+    this.setState({ssymbol: "", scategory: "", sshares: "", splatform: ""});
+  }
+  // handleSubmit: updates the tracked stocks array 
   handleSubmit(event) {
     let stockObject = {
       id: 1 + Math.random(), 
@@ -17,28 +33,40 @@ export class App extends Component {
       shares: this.state.sshares, 
       platform: this.state.splatform
     };
-    this.stocks.push(stockObject);
-    console.log(this.stocks);
+    if (document.getElementById("add-btn").innerHTML == "Add") {
+      this.setState({stocks: [...this.state.stocks, stockObject]});
+    } 
+    if (document.getElementById("add-btn").innerHTML == "Save") {
+      let newObject = {
+        id: this.state.currId, 
+        symbol: this.state.ssymbol, 
+        category: this.state.scategory, 
+        shares: this.state.sshares, 
+        platform: this.state.splatform
+      };
+      let filteredArray = this.state.stocks.filter(stock => stock.id != this.state.currId);
+      this.setState({stocks: [...filteredArray, newObject]});
+      this.setState({currId: ""});
+      document.getElementById("add-btn").innerHTML = "Add";
+    }
+    document.getElementById("add-btn").addEventListener("click", this.handleReset());
     event.preventDefault();
   }
+  // handleChange: keeps track of the input values specifed by user 
   handleChange(event) {
     let eventId = event.target.id;
     switch (eventId) {
       case "stock-symbol":
         this.setState({ssymbol: event.target.value});
-        console.log("Updated stock-symbol:", event.target.value);
         break;
       case "stock-category":
         this.setState({scategory: event.target.value});
-        console.log("Updated stock-category:", event.target.value);
         break;
       case "stock-shares":
         this.setState({sshares: event.target.value});
-        console.log("Updated stock-shares:", event.target.value);
         break;
       case "stock-platform":
         this.setState({splatform: event.target.value});
-        console.log("Updated stock-shares:", event.target.value);
         break;
       default: 
         console.log("No value added.");
@@ -58,44 +86,52 @@ export class App extends Component {
                 type="text" 
                 id="stock-symbol" 
                 placeholder="stock symbol" 
-                value={this.state.ssymbol} 
+                value={this.state.ssymbol}
                 onChange={this.handleChange.bind(this)}
               ></input>
               <input 
                 type="text" 
                 id="stock-category" 
                 placeholder="stock category" 
-                value={this.state.scategory} 
+                value={this.state.scategory}
                 onChange={this.handleChange.bind(this)}
               ></input>
               <input 
                 type="text" 
                 id="stock-shares" 
                 placeholder="stock shares" 
-                value={this.state.sshares} 
+                value={this.state.sshares}
                 onChange={this.handleChange.bind(this)}
               ></input>
               <input 
                 type="text" 
                 id="stock-platform" 
                 placeholder="stock platform" 
-                value={this.state.splatform} 
+                value={this.state.splatform}
                 onChange={this.handleChange.bind(this)}
               ></input>
               <button id="add-btn">Add</button>
             </div>
           </form>
+          {/* Renders an unordered list of the stocks array */}
           <ul id="stock-list">
-            {this.stocks.map(item => {
+            {this.state.stocks.map(item => {
               return(
-                <li id={item.id}>
-                  <p>{item.symbol},
-                     {item.category},
-                     {item.shares},
-                     {item.platform}
-                  </p>
-                  <button>Edit</button>
-                  <button>Delete</button>
+                <li key={item.id}>
+                  <p className="symbol" >{item.symbol}</p>
+                  <p className="category">{item.category}</p>
+                  <p className="shares">{item.shares}</p>
+                  <p className="platform">{item.platform}</p>
+                  <button 
+                    id={item.id} 
+                    className="edit-btn" 
+                    onClick={this.handleEdit.bind(this)}
+                  >Edit</button>
+                  <button 
+                    id={item.id} 
+                    className="delete-btn" 
+                    onClick={this.handleDelete.bind(this)}
+                  >Delete</button>
                 </li>
               )
             })}
